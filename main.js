634bbd85b95a1b8513a263c11467285a2e20e1ed
ayantiwari2022 <104711497+ayantiwari2022@users.noreply.github.com>
2022-10-01 14:40:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    img = loadImage("https://i.postimg.cc/zXYCc7wX/mustache-student-math-favorite-for-friday-the-the-1-1.png")
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
-    video = createCapture(VIDEO);
+    video =  createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(img, noseX - 23, noseY + 8, 50, 15)
 }
 
 function take_snapshot() {
@@ -20,15 +28,17 @@ function take_snapshot() {
 }
 
 function modelLoaded() {
-    console.log("Model is loaded! 😀");
-
+    console.log('poseNet is Initialized!😀');
 }
 
-function gotPoses(results) {
-    if (results.length > 0) {
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
         console.log(results);
-        console.log('nose x = ', results[0].pose.nose.x);
-        console.log('nose y = ', results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
     }
-
 }
